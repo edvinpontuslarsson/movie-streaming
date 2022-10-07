8c8e9d856ac1d36b487a56ecf14ba8f051ad8386
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-07 17:20:16 +0200

    logs env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,17 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    // TODO
+    // "In development, you will see two fetches in the Network tab. There is nothing wrong with that."
+    // perhaps use apollo server and come back to this, with caching
+    // https://beta.reactjs.org/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
+    console.log(process.env.REACT_APP_MOVIEDB_API_KEY);
+  }, []);
+
   return (
     <div className="App">
       <div className="button-wrap">
