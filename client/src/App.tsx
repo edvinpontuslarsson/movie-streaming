7a3d29a6f346ef4f60e227c0f203e8491912e228
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-07 19:51:41 +0200

    comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 function App() {
   useEffect(() => {
     // TODO
+    // because of strict mode
     // "In development, you will see two fetches in the Network tab. There is nothing wrong with that."
     // perhaps use apollo server and come back to this, with caching
     // https://beta.reactjs.org/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
