29890954fa20317d13faebc0b3a6f9eb20a55d64
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-07 17:22:03 +0200

    secret on btn click

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,13 +7,19 @@ function App() {
     // "In development, you will see two fetches in the Network tab. There is nothing wrong with that."
     // perhaps use apollo server and come back to this, with caching
     // https://beta.reactjs.org/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
-    console.log(process.env.REACT_APP_MOVIEDB_API_KEY);
   }, []);
 
   return (
     <div className="App">
       <div className="button-wrap">
-        <button className="fetch-button">Fetch</button>
+        <button
+          className="fetch-button"
+          onClick={() => {
+            console.log(process.env.REACT_APP_MOVIEDB_API_KEY);
+          }}
+        >
+          Fetch
+        </button>
       </div>
     </div>
   );
