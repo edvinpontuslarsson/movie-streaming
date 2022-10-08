8af9a85b22382b2fc1fe69754b45c5fd7c32f9ac
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-08 11:03:38 +0200

    removes old comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,6 @@ import { getTrendingMovies } from './api/api';
 import './App.css';
 
 function App() {
-  // TODO
-  // because of strict mode
-  // "In development, you will see two fetches in the Network tab. There is nothing wrong with that."
-  // perhaps use apollo server and come back to this, with caching
-  // https://beta.reactjs.org/learn/synchronizing-with-effects#how-to-handle-the-effect-firing-twice-in-development
-
-  // useEffect(() => {
-
-  // }, []);
-
   return (
     <div className="App">
       <div className="button-wrap">
--- client/src/interfaces/apiData.ts
+++ client/src/interfaces/apiData.ts
@@ -2,24 +2,6 @@ export interface ITrendingMovies {
   results: ITrendingMovieItem[];
 }
 
-// example item
-
-// adult: false
-// backdrop_path: '/l1ORpdYr8uFZ9MNYKoq1p6fIc4M.jpg'
-// genre_ids: (2)[(28, 14)]
-// id: 894205
-// media_type: 'movie'
-// original_language: 'en'
-// original_title: 'Werewolf by Night'
-// overview: 'On a dark and somber night, a secret cabal of monster hunters emerge from the shadows and gather at the foreboding Bloodstone Temple following the death of their leader. In a strange and macabre memorial to the leader’s life, the attendees are thrust into a mysterious and deadly competition for a powerful relic—a hunt that will ultimately bring them face to face with a dangerous monster.'
-// popularity: 94.959
-// poster_path: '/mvIvNKRIJPPS7WSFarFhOAGIVnU.jpg'
-// release_date: '2022-10-07'
-// title: 'Werewolf by Night'
-// video: false
-// vote_average: 7.7
-// vote_count: 91
-
 interface ITrendingMovieItem {
   adult: boolean;
   backdrop_path: string;
