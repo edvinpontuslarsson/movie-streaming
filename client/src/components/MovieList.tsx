5a7f4012b37f25b8616c1212b1fe1ae564771361
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-08 21:04:41 +0200

    initial flex

--- client/src/components/MovieList.tsx
+++ client/src/components/MovieList.tsx
@@ -12,37 +12,41 @@ export default function MovieList({
   const [chunkIndex, setChunkIndex] = useState(0);
 
   return (
-    <>
-      {chunkIndex > 0 && (
-        <button
-          onClick={() => {
-            const newIndex = chunkIndex - 1;
-            setChunkIndex(newIndex >= 0 ? newIndex : 0);
-          }}
-        >
-          Prev
-        </button>
-      )}
-      <br />
-      {movieListChunks.length > 0 &&
-        movieListChunks[chunkIndex].map((item) => (
-          <MoviePoster key={item.id} imgConfig={imgConfig} movie={item} />
-        ))}
-      <br />
-      {chunkIndex !== movieListChunks.length - 1 && (
-        <button
-          onClick={() => {
-            const newIndex = chunkIndex + 1;
-            setChunkIndex(
-              newIndex <= movieListChunks.length - 1
-                ? newIndex
-                : movieListChunks.length - 1
-            );
-          }}
-        >
-          Next
-        </button>
-      )}
-    </>
+    <div className="movie-section">
+      <div>
+        {chunkIndex > 0 && (
+          <button
+            onClick={() => {
+              const newIndex = chunkIndex - 1;
+              setChunkIndex(newIndex >= 0 ? newIndex : 0);
+            }}
+          >
+            Prev
+          </button>
+        )}
+      </div>
+      <div>
+        {movieListChunks.length > 0 &&
+          movieListChunks[chunkIndex].map((item) => (
+            <MoviePoster key={item.id} imgConfig={imgConfig} movie={item} />
+          ))}
+      </div>
+      <div>
+        {chunkIndex !== movieListChunks.length - 1 && (
+          <button
+            onClick={() => {
+              const newIndex = chunkIndex + 1;
+              setChunkIndex(
+                newIndex <= movieListChunks.length - 1
+                  ? newIndex
+                  : movieListChunks.length - 1
+              );
+            }}
+          >
+            Next
+          </button>
+        )}
+      </div>
+    </div>
   );
 }
