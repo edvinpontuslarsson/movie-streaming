6687e894713cd8677d7641f581d43c4f67da2063
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-08 15:21:16 +0200

    api return type

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,14 +1,12 @@
 // docs
 // https://developers.themoviedb.org/3/getting-started/introduction
 
-import { ITrendingMovies } from '../interfaces/apiData';
+import { IAPIConfig, ITrendingMovies } from '../interfaces/apiData';
 
 const apiURL = 'https://api.themoviedb.org/3';
 const apiKey = process.env.REACT_APP_MOVIEDB_API_KEY;
 
-// https://api.themoviedb.org/3/configuration?api_key=<<api_key>>
-
-export const getAPIConfig = () =>
+export const getAPIConfig = (): Promise<null | IAPIConfig> =>
   new Promise(async (resolve) => {
     try {
       const payload = await window.fetch(
@@ -18,7 +16,7 @@ export const getAPIConfig = () =>
 
       resolve(data);
     } catch (error) {
-      resolve({ images: {} });
+      resolve(null);
     }
   });
 
