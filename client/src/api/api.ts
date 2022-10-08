57b80f8cc1cb4a701adb0fe2219224fe445a1a6b
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-08 14:51:57 +0200

    get api config

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -6,6 +6,22 @@ import { ITrendingMovies } from '../interfaces/apiData';
 const apiURL = 'https://api.themoviedb.org/3';
 const apiKey = process.env.REACT_APP_MOVIEDB_API_KEY;
 
+// https://api.themoviedb.org/3/configuration?api_key=<<api_key>>
+
+export const getAPIConfig = () =>
+  new Promise(async (resolve) => {
+    try {
+      const payload = await window.fetch(
+        `${apiURL}/configuration?api_key=${apiKey}`
+      );
+      const data = await payload.json();
+
+      resolve(data);
+    } catch (error) {
+      resolve({ images: {} });
+    }
+  });
+
 export const getTrendingMovies = (
   timeWindow: 'day' | 'week' = 'day'
 ): Promise<ITrendingMovies> => {
