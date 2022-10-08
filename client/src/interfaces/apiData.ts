20a08648d6b1801f97bdf75b9d61876868dca30e
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-08 15:31:01 +0200

    updates interface

--- client/src/interfaces/apiData.ts
+++ client/src/interfaces/apiData.ts
@@ -1,17 +1,19 @@
-export interface ITrendingMovies {
-  results: ITrendingMovieItem[];
+export interface IAPIConfig {
+  images: IImgConfig;
 }
 
-export interface IAPIConfig {
-  images: {
-    backdrop_sizes: string[] //  ['w300', 'w780', 'w1280', 'original'];
-    base_url: string // 'http://image.tmdb.org/t/p/';
-    logo_sizes: string[] // ['w45', 'w92', 'w154', 'w185', 'w300', 'w500', 'original'];
-    poster_sizes: string[] // ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'];
-    profile_sizes: string[] // ['w45', 'w185', 'h632', 'original'];
-    secure_base_url: string // 'https://image.tmdb.org/t/p/';
-    still_sizes: string[] // ['w92', 'w185', 'w300', 'original'];
-  };
+export interface IImgConfig {
+  backdrop_sizes: string[]; //  ['w300', 'w780', 'w1280', 'original'];
+  base_url: string; // 'http://image.tmdb.org/t/p/';
+  logo_sizes: string[]; // ['w45', 'w92', 'w154', 'w185', 'w300', 'w500', 'original'];
+  poster_sizes: string[]; // ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'];
+  profile_sizes: string[]; // ['w45', 'w185', 'h632', 'original'];
+  secure_base_url: string; // 'https://image.tmdb.org/t/p/';
+  still_sizes: string[]; // ['w92', 'w185', 'w300', 'original'];
+}
+
+export interface ITrendingMovies {
+  results: ITrendingMovieItem[];
 }
 
 export interface ITrendingMovieItem {
