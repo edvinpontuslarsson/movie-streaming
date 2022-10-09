9530994019ffaed6940f36e30d84d0694cf2e4e9
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-09 11:05:51 +0200

    more styling

--- client/src/components/MoviePoster.tsx
+++ client/src/components/MoviePoster.tsx
@@ -11,19 +11,19 @@ export default function MoviePoster({
   const [onHover, setOnHover] = useState(false);
 
   return (
-    <div>
-      <a
-        href="#!"
-        onMouseEnter={() => setOnHover(true)}
-        onFocus={() => setOnHover(true)}
-        onMouseLeave={() => setOnHover(false)}
-        onBlur={() => setOnHover(false)}
-      >
+    <div
+      className="movie-poster-link-wrap"
+      onMouseEnter={() => setOnHover(true)}
+      onFocus={() => setOnHover(true)}
+      onMouseLeave={() => setOnHover(false)}
+      onBlur={() => setOnHover(false)}
+    >
+      <a href="#!">
         <img
           src={`${imgConfig.secure_base_url}${imgConfig.poster_sizes[1]}/${movie.poster_path}`}
           alt={`Poster for movie ${movie.title}`}
         />
-        {onHover && <p className="movie-poster-link">{movie.title}</p>}
+        {onHover && <p className="movie-poster-link-text">{movie.title}</p>}
       </a>
     </div>
   );
