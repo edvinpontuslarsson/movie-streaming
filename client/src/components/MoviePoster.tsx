2a680330b4e40749a92afdfbe1ca8db63b49a063
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-09 10:57:02 +0200

    link white as underline color

--- client/src/components/MoviePoster.tsx
+++ client/src/components/MoviePoster.tsx
@@ -11,18 +11,20 @@ export default function MoviePoster({
   const [onHover, setOnHover] = useState(false);
 
   return (
-    <a
-      href="#!"
-      onMouseEnter={() => setOnHover(true)}
-      onFocus={() => setOnHover(true)}
-      onMouseLeave={() => setOnHover(false)}
-      onBlur={() => setOnHover(false)}
-    >
-      <img
-        src={`${imgConfig.secure_base_url}${imgConfig.poster_sizes[1]}/${movie.poster_path}`}
-        alt={`Poster for movie ${movie.title}`}
-      />
-      {onHover && <p style={{ color: 'white' }}>{movie.title}</p>}
-    </a>
+    <div>
+      <a
+        href="#!"
+        onMouseEnter={() => setOnHover(true)}
+        onFocus={() => setOnHover(true)}
+        onMouseLeave={() => setOnHover(false)}
+        onBlur={() => setOnHover(false)}
+      >
+        <img
+          src={`${imgConfig.secure_base_url}${imgConfig.poster_sizes[1]}/${movie.poster_path}`}
+          alt={`Poster for movie ${movie.title}`}
+        />
+        {onHover && <p className="movie-poster-link">{movie.title}</p>}
+      </a>
+    </div>
   );
 }
