badb93608b4372c4b4c24655a564e5431fbe9f56
edvinpontuslarsson <edvinpontuslarsson@gmail.com>
2022-10-09 16:36:19 +0200

    simple movie cards

--- client/src/components/MoviePoster.tsx
+++ client/src/components/MoviePoster.tsx
@@ -1,9 +1,6 @@
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-import { useState } from 'react';
 import { ITrendingMovieItem, IImgConfig } from '../interfaces/apiData';
 
 export default function MoviePoster({
@@ -13,42 +10,8 @@ export default function MoviePoster({
   movie: ITrendingMovieItem;
   imgConfig: IImgConfig;
 }) {
-  const [onHover, setOnHover] = useState(false);
-
   const stringInitialPosterSize = imgConfig.poster_sizes[1].split('w')[1];
-
   const width = parseInt(stringInitialPosterSize);
-
-  // import * as React from 'react';
-  // import Card from '@mui/material/Card';
-  // import CardContent from '@mui/material/CardContent';
-  // import CardMedia from '@mui/material/CardMedia';
-  // import Typography from '@mui/material/Typography';
-  // import { CardActionArea } from '@mui/material';
-
-  // export default function ActionAreaCard() {
-  //   return (
-  //     <Card sx={{ maxWidth: 345 }}>
-  //       <CardActionArea>
-  //         <CardMedia
-  //           component="img"
-  //           height="140"
-  //           image="/static/images/cards/contemplative-reptile.jpg"
-  //           alt="green iguana"
-  //         />
-  //         <CardContent>
-  //           <Typography gutterBottom variant="h5" component="div">
-  //             Lizard
-  //           </Typography>
-  //           <Typography variant="body2" color="text.secondary">
-  //             Lizards are a widespread group of squamate reptiles, with over 6,000
-  //             species, ranging across all continents except Antarctica
-  //           </Typography>
-  //         </CardContent>
-  //       </CardActionArea>
-  //     </Card>
-  //   );
-  // }
 
   return (
     <Card>
@@ -57,7 +20,7 @@ export default function MoviePoster({
           src={`${imgConfig.secure_base_url}${imgConfig.poster_sizes[2]}/${movie.poster_path}`}
           alt={`Poster for movie ${movie.title}`}
         /> */}
-        <CardMedia 
+        <CardMedia
           component="img"
           width={width}
           height={225}
